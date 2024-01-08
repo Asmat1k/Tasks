1ca88ec02905f4f86edb99a6747b4f7e4909326f
Asmat1k <dobrovti@mail.ru>
2024-01-08 19:16:21 +0300

    feat: add task reverse-int

---
+++ reverse-int-master/src/index.js
@@ -0,0 +1,6 @@
+module.exports = function reverse (n) {
+    if(n>0)
+        return n.toString().split('').reverse().join('');
+    else 
+        return (n*(-1)).toString().split('').reverse().join('');
+}
