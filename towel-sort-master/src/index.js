6965bdf40815cde5fa56745d32decd10517cbc4b
Asmat1k <dobrovti@mail.ru>
2024-01-08 19:20:34 +0300

    feat: add towel-sort task

---
+++ towel-sort-master/src/index.js
@@ -0,0 +1,16 @@
+module.exports = function towelSort (matrix) {
+  let res = [];
+  if(matrix == undefined)
+    return res;
+  else
+    for(let i=0; i<matrix.length; i++) {
+      if(i%2!=0)
+        matrix[i].sort(function(a,b) {
+          return b-a;
+        });
+      for(let j=0; j<matrix[i].length; j++) 
+        res.push(matrix[i][j]);
+    }
+  return res;
+}
+
