5ea5c49a6f775b13ea6ee2179b2c16eb163926e2
Asmat1k <dobrovti@mail.ru>
2024-01-08 19:37:51 +0300

    feat: add basic-js-ds task

---
+++ basic-js-ds-master/src/queue.js
@@ -0,0 +1,51 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+// const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    let list = {
+      value,
+      next: null,
+    };
+    
+    if(!this.head) {
+      this.head = list;
+      this.tail = list;
+    }
+    else {
+      this.tail.next = list;
+      this.tail = this.tail.next; 
+    }
+  }
+
+  dequeue() {
+    let first = this.head;
+    this.head = this.head.next;
+    return first.value;
+  }
+}
+
+module.exports = {
+  Queue
+};
